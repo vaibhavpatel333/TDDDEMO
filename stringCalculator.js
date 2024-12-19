7f9db17290991b88fac1166aa76e224e93b60286
vaibhavp <vaibhavpatel2017@gmail.com>
2024-12-19 12:29:17 +0530

    implement sum functionality for two numbers and single number

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,12 @@
 export const add = (numbers)=>{
     if (numbers === '') return 0;
-    return 0;
+    let delimiter = /[,]/;
+    let numbersToAdd = numbers;
+
+    const nums = numbersToAdd.split(delimiter)
+    .map(str => str.trim())
+    .filter(str => str !== '') 
+    .map(Number);
+
+    return nums.reduce((sum, num) => sum + num, 0);
 }
